e6ac228db1d806a3db3e264051939bd8f64c30b0
Anastasiia Shlapak <112555283+kelly9323@users.noreply.github.com>
2022-11-25 23:38:49 +0200

    update task

--- script.js
+++ script.js
@@ -8,32 +8,29 @@ const arrNums = [1, 9, 5, 6 , 7, 9, 4, 6]; => індекс 5
 const arrNums = [1, 9, 5, 6, 7, 9, 4, 6];
 
 const maxElementArray = function (array) {
-  let max;
-  let min = array[0];
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] < min) {
-      min = array[i];
-    } else {
-      max = array[i] - 1;
+  let i, max;
+  max = 0;
+  for (i = 0; i < array.length; i++) {
+    if (array[i] >= array[max]) {
+      max = i;
     }
   }
   return max;
 };
-
 console.log(maxElementArray(arrNums));
 
 //* * повертати кількість максимальних(однакових) елементів
 //const arrNums = [1, 9, 5, 6 , 7, 9, 4, 6]; =>кількість  2
 
 function returnNumberOfMaxEl(array) {
+  let maxIndex = maxElementArray(arrNums);
   let count = 0;
-  let max = 0;
   for (let i = 0; i < array.length; i++) {
-    if (array[i] > max) {
-      max = array[i];
+    if (array[i] === array[maxIndex]) {
       count++;
     }
   }
   return count;
 }
+// debugger
 console.log(returnNumberOfMaxEl(arrNums));
